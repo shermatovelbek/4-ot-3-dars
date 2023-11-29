0b34ba06936f23a3b2318f0aaedadb0d0d9c203c
shermatovelbek <shermatovelbek772@gmail.com>
2023-11-29 17:32:28 +0500

    fayllar yaratildi

---
+++ js/main.js
@@ -0,0 +1,38 @@
+    // bu misol emas !
+    // let a = 55;
+    // let b = 66;
+    // let res;
+
+    // a = (a % 10) - a % 10 / 10;
+    // b = ( b % 10) - b % 10 / 10;
+
+    // if (a > b){
+    //     res = a;
+    // } else{
+    //     res = b;
+    // }
+    // console.log(res);
+
+
+
+    //2-misol
+
+    // let a = 123;
+    // let max ;
+    // let b = a % 10;
+    // let o = a % 100;
+
+
+    //1-misol
+
+    let a = 94;
+    let b = 96 ;
+    let res;
+    res = (a % 100) - a % 10 / 10;
+    res = (b % 100) - b % 10 / 10;
+    console.log(res);
+    if (a > b){
+        a = res;
+    } 
+    console.log(res);
+
