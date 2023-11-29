a68ca02629a2513f6a6c0e67639b2a76c7dc4625
shermatovelbek <shermatovelbek772@gmail.com>
2023-11-29 18:46:40 +0500

    js fayllarda misollar ishlanilmoqda

--- js/main.js
+++ js/main.js
@@ -42,18 +42,124 @@
 
     //2-misol
 
-     let a = 190;
-     let max;
-    let b = a % 10;
-    let o = (a % 100) - o / 10;
-    let y = (a / 100) - (a % 100) / 100 ;
+    //  let a = 190;
+    //  let max;
+    // let b = a % 10;
+    // let o = (a % 100) - o / 10;
+    // let y = (a / 100) - (a % 100) / 100 ;
 
-    if(b > o){
-        max = b;
-    } else{
-        max = o;
+    // if(b > o){
+    //     max = b;
+    // } else{
+    //     max = o;
+    // }
+    // if (max < y){
+    //     max = y;
+    // }
+    // console.log(max);
+
+    //3-misol
+
+    // let a = 342 ;
+    // let o = (a % 100) - o / 10;
+    // if (o % 3 == 0){
+    //     o = rost;
+    // }  else{
+    //     o = yolgon
+    // };
+    // console.log(o);
+
+    //4-misol
+    
+
+    // let a = 20;
+    // let b = 35;
+    // let c = 95;
+    // if(a > b){
+    //     console.log(a);
+    // }   else (b);
+    // if(b > c){
+    //     console.log(b);
+    // }    else (c);
+
+    // console.log(c)
+
+
+    //5-misol
+    // let a = 20;
+    // let b = 60;
+    // let c = 70;
+    // let d = 50;
+    // let max;
+    // if (a > b ){
+    //     max = a;
+    // } else {
+    //     max = b;
+    // }
+    // if(b > c){
+    //     max = b;
+    // } else{
+    //     max = c;
+    // }
+    // if (c > d ){
+    //     max = c;
+    // }   else {
+    //     max = d;
+    // }
+    // console.log(max);
+
+
+
+
+    //6-misol
+    // let a = 120 ;
+    // let b = 160;
+    // let c = 190;
+    // let max;
+    // if(a >= b ){
+    //     max = a;
+    // }    else{
+    //     max = b;
+    // }
+
+    // if(b >= c){
+    //     max = c;
+    // }   else {
+    //     max = b;
+    // };
+    // console.log(max)
+
+
+    //7-misol
+    let a = 250;
+    let b = 230;
+    let c = 190;
+    let d = 150;
+    let e = 180;
+    let res;
+    if(a % 3 == 0){
+        res = a;
+    } else {
+        res = "bu son 3 ga karrali emas";
     }
-    if (max < y){
-        max = y;
+    if(b % 3 == 0){
+        res = b;
+    }  else {
+        res = "bu son 3 ga karrali emas";
     }
-    console.log(max);
+    if(c % 3 == 0){
+        res = c;
+    }  else {
+        res = "bu son 3 ga karrali emas";
+    }
+    if(d % 3 == 0){
+        res = d;
+    }  else {
+        res = "bu son 3 ga karrali emas";
+    }
+    if(e % 3 == 0){
+        res = e;
+    }  else {
+        res = "bu son 3 ga karrali emas";
+    }
+    console.log(res)
